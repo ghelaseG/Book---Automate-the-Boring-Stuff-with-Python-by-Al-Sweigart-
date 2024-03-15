f6874d7c0dc2fe3fefb408eb22b67067cf29c123
Georgian Ghelase <ghelase.georgian@gmail.com>
2024-03-15 20:07:16 +0000

    Create 6.py

---
+++ Fantasy Game Inventory + List to Dictionary Function/6.py
@@ -0,0 +1,22 @@
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+def displayInventory(inventory):
+    total_items = 0
+    for item, quantity in inventory.items():
+        print(str(quantity) + ' ' + item)
+        total_items += quantity
+    print("Total number of items: "+str(total_items))
+
+displayInventory(stuff)
+
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        inventory.setdefault(item, 0)
+        inventory[item] += 1
+    return inventory
+
+inv = {'gold coin': 42, 'rope': 1}
+displayInventory(inv)
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+addToInventory(inv, dragonLoot)
+displayInventory(inv)
